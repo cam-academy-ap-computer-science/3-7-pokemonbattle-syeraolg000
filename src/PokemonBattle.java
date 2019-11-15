be223bf6648706e56bb496e2d1e57a9a46b17a70
syeraolg000 <55398895+syeraolg000@users.noreply.github.com>
2019-11-15 14:51:27 -0800

    done late

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,15 +1,4 @@
- /* Sarah Gilliam
- * Grading Comments:   Incomplete program - 11/25
- *  
- *  RUBRIC Unit 3 = 25 pts
- * 10 pts - Correct Output (must compile and run)   5/10
- * 5 pts - Proper Use of Scanner, and decent User Interface  3/5
- * 3 pts - Proper Use of Return       0/2
- * 3 pts - Proper User of Parameter   0/2
- * 2 pts - Pseudocode and comments    1/2   
- * 2 pts - Consistent Layout and Style following Style Guidelines  2/2
- */
-import java.util.*;
+ import java.util.*;
 /*Main
  * call battleStart
  * call damage
@@ -19,50 +8,118 @@ import java.util.*;
  * ask for pokemon name
  * add in the name into the next line and the line after that 
  * return pokemon
+ * DAMAGE
+ * Ask user for stats
+ * calculate the damage
+ * return damage + HP value.
+ * STATSTABLE.
+ * print out stats with new HP value.
  */
 
 public class PokemonBattle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		battleStart();
-		damage();
-		statsTable();
+		int HP;
+		String pokemon = battleStart();
+		HP=damage(pokemon);
+		statsTable(HP, pokemon);
+
 	}
 	public static String battleStart() {
 		Scanner console = new Scanner(System.in);
 		System.out.println("Another trainer is issuing a challenge!");
-		
-		System.out.println("Zebstrika appeared.");
-		
-		System.out.println("Which pokemon do you choose?");
-		
-		String pokemon = console.next();
-		
-		System.out.println("You chose " + pokemon + "!");
-	
-		System.out.println("It's a pokemon battle between " + pokemon + " and Zebstrika! Go!");
-		
-		return pokemon;
-		
-		}
-	public static void damage() {
-		Scanner console = new Scanner(System.in);
-		
-		System.out.println("Zebstrika used Thunderbolt!");
-		
-		String pokemon = console.next();
-		
-		System.out.println("Trainer what are your"+ pokemon + "stats");
-		
-	
-		}
-	public static void statsTable() {
 
+		System.out.println("Zebstrika appeared.");
+
+		System.out.println("Which pokemon do you choose?");
+
+		String pokemon = console.next();
+
+		System.out.println("You chose " + pokemon + "!");
+
+		System.out.println("It's a pokemon battle between " + pokemon + " and Zebstrika! Go!");
+
+		return pokemon;
+
+		}
+	public static final int damage(String pokemon) {
+		Scanner console = new Scanner(System.in);
+
+		System.out.println("Zebstrika used Thunderbolt!");
+
+		System.out.println("Trainer what are your "+ pokemon + " stats");
+		
+		System.out.println("Level: ");
+		
+		int level = console.nextInt();
+		
+		System.out.println("Attack: ");
+		
+		int attack = console.nextInt();
+		
+		System.out.println("Defense: ");
+		
+		int defense = console.nextInt();
+		
+		System.out.println("Base: ");
+		
+		int base = console.nextInt();
+		
+		System.out.println("STAB");
+		
+		int STAB = console.nextInt();
+		
+		System.out.println("HP ");
+		
+		int HP = console.nextInt();
+		
+		int modifier = (int)(STAB * Math.random());
+		
+		int damage;
+		
+		damage = (((2 * level + 10)/250 + (attack/defense) * base + 2) * modifier);
+		
+		System.out.println(pokemon + " sustained " + damage + " points damage");
+		
+		System.out.println("HP, after damage, are now " + (damage + HP));
+		
+		return HP + damage;
+
+		
+
+	
+
+		
+		
+
+		}
+	public static void statsTable(int HP, String pokemon) {
+		
+		System.out.println("Name: " + pokemon);
+		
+		System.out.println("Level: 1");
+		
+		System.out.println("----------------------------------");
+		
+		System.out.println("HP: " + HP);
+		
+		System.out.println("Attack: 2");
+		
+		System.out.println("Defense: 2");
+		
+		System.out.println("SPD ATK: 8");
+		
+		System.out.println("SPD DEF: 6");
+		
+		System.out.println("Speed: 4 ");
+		
 
 	}
-	
+
 }
+
+	
 
 
 
